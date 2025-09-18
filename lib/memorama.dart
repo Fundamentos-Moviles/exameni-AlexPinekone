0467f6f77f2fa2f4ffab0c107fa0f7d75e39f4ad
AlexPinekone <127763929+AlexPinekone@users.noreply.github.com>
2025-09-18 15:39:32 -0600

    feat(front): Memorama logic
    -Toda la logica del giro de tarjetas. Si son iguales se quedan , si no vuelven a girar

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -9,33 +9,40 @@ class Memorama extends StatefulWidget {
 
 List<int> numeros = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9];
 List<List<int>> matriz = [];
+List<List<int>> girados = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+];
+bool stop = false;
+List<int> card1 = [-1, -1];
+List<int> card2 = [-1, -1];
 
 class _MemoramaState extends State<Memorama> {
-  var c1 = 1;
-
   @override
   void initState() {
     super.initState();
-    MatrizInit();
+    MatrizInit(); // Se inicializa una vez
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Column(
         children: [
           const SizedBox(height: 40),
           const Text("Piña Becerril Manuel Alejandro"),
 
-          //Tablero
+          // Tablero
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 for (int fila = 0; fila < 5; fila++) ...[
-                  FilaM(matriz[fila]),
-                  const SizedBox(height: 2),
+                  FilaM(fila),
+                  const SizedBox(height: 4),
                 ],
               ],
             ),
@@ -45,33 +52,47 @@ class _MemoramaState extends State<Memorama> {
     );
   }
 
-  //Fila
-  Row FilaM(List<int> numcolores) {
+  // Genera una fila de casillas
+  Row FilaM(int fila) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: numcolores
-          .map(
-            (num) => Flexible(
-          child: Padding(
+      children: [
+        for (int col = 0; col < 4; col++)
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 2),
             child: Casilla(
-              color: getColorFromNumber(num),
+              fila: fila,
+              col: col,
+              value: matriz[fila][col],
+              color: getColorFromNumber(matriz[fila][col]),
               onTap: () {
                 setState(() {
-                  //c1 = color.value;
+                  debugPrint("Tocaste casilla [$fila,$col] valor ${matriz[fila][col]}");
+                  if(stop == false) {
+                    if (girados[fila][col] == 0) {
+                      girados[fila][col] = 1;
+                      if (card1[0] == -1) {
+                        card1[0] = fila;
+                        card1[1] = col;
+                      } else {
+                        card2[0] = fila;
+                        card2[1] = col;
+                        checkPair();
+                      }
+                    }
+                  }else{
+                      checkPair();
+                  }
                 });
-                debugPrint("Tocaste casilla color $num");
               },
             ),
           ),
-        ),
-      )
-          .toList(),
+      ],
     );
   }
 
-  ///Casilla individual
-  InkWell Casilla({required Color color, required VoidCallback onTap}) {
+  // Casilla individual
+  InkWell Casilla({required int fila, required int col, required int value, required Color color, required VoidCallback onTap,}) {
     return InkWell(
       onTap: onTap,
       child: SizedBox(
@@ -79,7 +100,7 @@ class _MemoramaState extends State<Memorama> {
         height: 120,
         child: Container(
           decoration: BoxDecoration(
-            color: color,
+            color: girados[fila][col] == 0 ? Colors.black : color,
             borderRadius: BorderRadius.circular(6),
           ),
         ),
@@ -91,17 +112,21 @@ class _MemoramaState extends State<Memorama> {
   void MatrizInit(){
 
     numeros.shuffle();
+    matriz.clear();
 
     int cont = 0;
 
     for (int fila = 0; fila < 5; fila++) {
-      for(int colu = 0; colu <4; colu++){
-        matriz[fila][colu] = numeros[cont];
+      List<int> filaTemp = [];
+      for (int col = 0; col < 4; col++) {
+        filaTemp.add(numeros[cont]);
         cont++;
       }
+      matriz.add(filaTemp);
     }
   }
 
+  // Mapea número a color
   Color getColorFromNumber(int n) {
     switch (n) {
       case 0: return Colors.blue;
@@ -116,5 +141,34 @@ class _MemoramaState extends State<Memorama> {
       case 9: return Colors.cyan;
       default: return Colors.grey;
     }
+  }
+
+  bool checkPair(){
+
+    bool res;
+
+    if(matriz[card1[0]][card1[1]] == matriz[card2[0]][card2[1]]) {
+      stop = false;
+      res = true;
+      card1[0] = -1;
+      card1[1] = -1;
+      card2[0] = -1;
+      card2[1] = -1;
+    }else {
+      if(stop == true){
+        stop = false;
+        girados[card1[0]][card1[1]] = 0;
+        girados[card2[0]][card2[1]] = 0;
+        card1[0] = -1;
+        card1[1] = -1;
+        card2[0] = -1;
+        card2[1] = -1;
+      }else{
+        stop = true;
+      }
+      res = false;
+    }
+
+    return res;
   }
 }
